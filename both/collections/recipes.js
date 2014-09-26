e47fe9361eedbd551f5032162b8184c0c20a6c45
Joshua Ohlman <codewriterohs@gmail.com>
2014-09-26 13:58:18 -0500

    Add validation rules

--- both/collections/recipes.js
+++ both/collections/recipes.js
@@ -3,24 +3,24 @@ Recipes = new Meteor.Collection('recipes');
 CollectionRule.attachSchema(Recipes, {
 	name: 'recipe'
 	, schema: {
-		name: []
-		, description: []
+		name: [Rule.isString, Rule.notEmpty, Rule.maxLength(120)]
+		, description: [Rule.isString, Rule.maxLength(255)]
 		, categories: {
 			isArray: true
-			, rules: []
+			, rules: [Rule.isString, Rule.minLength(3), Rule.maxLength(20)]
 		}
 		, tags: {
 			isArray: true
-			, rules: []
+			, rules: [Rule.isString, Rule.minLength(3), Rule.maxLength(20)]
 		}
 		, steps: {
 			isArray: true
 			, schema: {
-				ingredient: []
-				, amount: []
-				, unit: []
-				, prep: []
-				, comments: []
+				ingredient: [Rule.isString, Rule.minLength(2), Rule.maxLength(120)]
+				, amount: [Rule.isNumber, Rule.minValue(0)]
+				, unit: [Rule.isString]
+				, prep: [Rule.isString, Rule.maxLength(255)]
+				, comments: [Rule.isString]
 			}
 		}
 	}
